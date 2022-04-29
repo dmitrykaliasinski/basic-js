2588afadf16784103e207364f50332d13914ddb0
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-04-29 18:20:29 +0300

    made some more

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const arr1 = s1.split("").sort();
+  const arr2 = s2.split("").sort();
+  let count = 0;
+  let sameChar = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) {
+      if (sameChar.includes(arr1[i])) {
+        continue;
+      } else {
+        sameChar.push(arr1[i]);
+        let min = Math.min(
+          arr1.filter((item) => item === s1[i]).length,
+          arr2.filter((item) => item === s1[i]).length
+        );
+        count += min;
+      }
+    }
+  }
+  return count;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,46 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  return arr
+    .map((el, i) =>
+      el === "--double-prev" && i > 0
+        ? arr[i - 1]
+        : el && el === "--double-next" && i < arr[arr.length - 1]
+        ? arr[i + 1]
+        : el && el === "--discard-prev" && i > 0
+        ? arr.splice(arr[i - 1], 1)
+        : el && el === "--discard-next" && i < arr[arr.length - 1]
+        ? arr.splice(arr[i + 1], 1)
+        : el
+    )
+    .filter(
+      (el) =>
+        ![
+          "--discard-prev",
+          "--discard-next",
+          "--double-prev",
+          "--double-next",
+        ].includes(el)
+    );
 }
 
 module.exports = {
-  transform
+  transform,
 };
